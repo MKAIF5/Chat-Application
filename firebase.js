4a058e736eff553eecb653e07dc7ea7245393b13
mohammed kaif <mkaif555123@gmail.com>
2024-08-08 01:36:17 +0500

    firebase initialize

---
+++ firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-database.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAVtrxI3qVFxFG3rhcAm3PNq1O5CoZZozc",
+  authDomain: "chat-application-53142.firebaseapp.com",
+  projectId: "chat-application-53142",
+  storageBucket: "chat-application-53142.appspot.com",
+  messagingSenderId: "934311044892",
+  appId: "1:934311044892:web:09573d2bd9fe158cb7bcd9"
+};
+
+const app = initializeApp(firebaseConfig);
